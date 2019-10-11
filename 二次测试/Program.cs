9d23a81c0d28296500117595d5b6b54c9ff348a2
zbino <740090794>
2019-10-11 20:51:57 +0800

    添加项目文件。

---
+++ 二次测试/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 二次测试
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("期待测试成功");
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
